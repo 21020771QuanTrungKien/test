a9ffae8b23bbf5f71f1d3f54282658fd095174a3
21020771QuanTrungKien <qtkk20a@gmail.com>
2022-05-25 00:10:45 +0700

    Add project files.

---
+++ FlappyCow/PlayerObject.cpp
@@ -0,0 +1,63 @@
+
+#include "PlayerObject.h"
+
+PlayerObject::PlayerObject()
+{
+    is_falling_ = false;
+    y_val_ = 0;
+    gravity = 0.4;
+}
+
+PlayerObject::~PlayerObject()
+{
+  Free();
+}
+
+void PlayerObject::HandleInputAction(SDL_Event events, SDL_Renderer* screen) 
+{
+  if( events.type == SDL_KEYDOWN )
+  {
+    switch( events.key.keysym.sym )
+    {
+    case SDLK_SPACE:
+      {
+        if (is_falling_ == false)
+        {
+            y_val_ = -6;
+        }
+      }
+    }
+  }
+  
+}
+
+
+bool PlayerObject::LoadImg(std::string path, SDL_Renderer* screen)
+{
+  bool ret = BaseObject::LoadImageFile(path, screen);
+  return ret;
+}
+
+void PlayerObject::Show(SDL_Renderer* des)
+{
+   BaseObject::Render(des);
+}
+
+void PlayerObject::DoFalling(SDL_Renderer* des)
+{
+    y_val_ += gravity;
+    rect_.y += y_val_;
+    if ((rect_.y + rect_.h) >= GROUND_MAP)
+    {
+        LoadImg("img//cow2.png", des);
+        is_falling_ = true;
+        DoGround(des);
+    }
+}
+
+void PlayerObject::DoGround(SDL_Renderer* screen)
+{
+    y_val_ = 0;
+    rect_.y = GROUND_MAP - rect_.h;
+    is_die_ = true;
+}
